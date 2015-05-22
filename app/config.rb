9afc9749730a93111be01a401108116935ccbc24
idready <you_sombo@hotmail.fr>
2015-05-22 21:16:54 +0200

    Test Heroku deploy

--- app/config.rb
+++ app/config.rb
@@ -26,3 +26,26 @@ relative_assets = true
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+require 'sass'
+require 'cgi'
+
+module Sass::Script::Functions
+
+  def inline_svg_image(path)
+    real_path = File.join(Compass.configuration.images_path, path.value)
+    svg = data(real_path)
+    encoded_svg = CGI::escape(svg).gsub('+', '%20')
+    data_url = "url('data:image/svg+xml;charset=utf-8," + encoded_svg + "')"
+    Sass::Script::String.new(data_url)
+  end
+
+private
+
+  def data(real_path)
+    if File.readable?(real_path)
+      File.open(real_path, "rb") {|io| io.read}
+    else
+      raise Compass::Error, "File not found or cannot be read: #{real_path}"
+    end
+  end
